830579b1860cf23f183d3653f2b56c39b3dacff3
jeffreynerona <jeffnerona@gmail.com>
2017-10-30 13:19:31 +0800

    Added Searchbar Container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar'
 
 class App extends Component {
   render() {
     return (
       <div>
-      Test
+      	<SearchBar/>
       </div>
     );
   }
---
+++ src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { term: '' };
+	}
+
+	onInputChange = (e) => {
+		this.setState({term: e.target.value});
+	}
+
+	onFormSubmit = (e) => {
+		e.preventDefault();
+
+		// fetch weather data
+	}
+
+	render() {
+		return(
+			<form onSubmit={this.onFormSubmit} className="input-group">
+				<input 
+					placeholder="Enter City"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
+				<span className="input-group-btn">
+					<button type="submit" className="btn btn-secondary">Submit</button>
+				</span>
+			</form>
+			);
+	}
+}
