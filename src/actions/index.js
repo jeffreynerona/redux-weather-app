e308b74ae036ed45dfae6474cb1c5c48474cbdf5
jeffreynerona <jeffnerona@gmail.com>
2017-10-30 14:48:47 +0800

    Connected the fetch weather action to the search bar and added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const API_KEY = '9e753fe9794a0349189374c7fb329bfc';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
 
@@ -5,8 +7,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},ph`;
+	const request = axios.get(url);
 
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
+		payload: request
 	};
 }
