83d60d57b17ce0346e9aa1d6c7fb6f651493536a
jeffreynerona <jeffnerona@gmail.com>
2017-10-31 12:03:02 +0800

    Added Charts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = '9e753fe9794a0349189374c7fb329bfc';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&APPID=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
